218983f17bf24d5cb7b8df312659a2d54c3f67a2
Dastonbek <yakubovdastonbek@gmail.com>
2022-12-15 10:54:50 +0500

    bad tasks

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        float a = 0.5f;
+        float b = 0.5f;
+        System.out.println(((9*a*a-5*b+a-5)*(a+b-4*a*b)/2));
     }
 }
